138889ce680be16644ee418de15a034d84d8d487
Ryan Yogan <ryanyogan@gmail.com>
2024-10-15 13:02:44 -0500

    feat // a proper config with some notes for yordi

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,40 @@
 import type { NextConfig } from "next";
+// import path from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Recommended: this will reduce output
+  // Docker image size by 80%+
+
+  output: "standalone",
+
+  // Optional: bring your own cache handler
+  // cacheHandler: path.resolve('./cache-handler.mjs'),
+  // cacheMaxMemorySize: 0, // Disable default in-memory caching (LRU, Next 15 feature)
+
+  images: {
+    // Optional: use a different optimization service, Sharp is built-into Next 15
+    // loader: 'custom',
+    // loaderFile: './image-loader.ts',
+    //
+    // Here be Sharp, which is built-into `next start` 15 only....
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+        search: "",
+      },
+    ],
+  },
+  // Nginx will do gzip compression. We gotta disable
+  // compression here so we can prevent buffering
+  // streaming responses... hmmm, interesting future lol
+  compress: false,
+  // look into this later
+  // Optional: override the default (1 year) `stale-while-revalidate`
+  // header time for static pages
+  // swrDelta: 3600 // seconds
 };
 
 export default nextConfig;
