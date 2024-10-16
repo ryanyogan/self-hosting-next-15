31fd5bfa364596f5f23fcadee587b232e9018e97
Ryan Yogan <ryanyogan@gmail.com>
2024-10-16 00:23:31 -0500

    feat // migrator

---
+++ src/lib/db/migrate.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import path from "path";
+import { client, db } from "./drizzle";
+
+dotenv.config();
+
+async function main() {
+  await migrate(db, {
+    migrationsFolder: path.join(process.cwd(), "/lib/db/migrations"),
+  });
+
+  console.log("Migration complete");
+  await client.end();
+}
+
+main();
