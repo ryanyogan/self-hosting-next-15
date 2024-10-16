a0b98fec69fd09e6e598bf8459f6404bd7b5f368
Ryan Yogan <ryanyogan@gmail.com>
2024-10-16 00:20:38 -0500

    feat // schema & config

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -130,3 +130,16 @@ export type TeamDataWithMembers = Team & {
     user: Pick<User, "id" | "name" | "email">;
   })[];
 };
+
+export enum ActivityType {
+  SIGN_UP = "SIGN_UP",
+  SIGN_IN = "SIGN_IN",
+  SIGN_OUT = "SIGN_OUT",
+  UPDATE_PASSWORD = "UPDATE_PASSWORD",
+  DELETE_ACCOUNT = "DELETE_ACCOUNT",
+  UPDATE_ACCOUNT = "UPDATE_ACCOUNT",
+  CREATE_TEAM = "CREATE_TEAM",
+  REMOVE_TEAM_MEMBER = "REMOVE_TEAM_MEMBER",
+  INVITE_TEAM_MEMBER = "INVITE_TEAM_MEMBER",
+  ACCEPT_INVITATION = "ACCEPT_INVITATION",
+}
